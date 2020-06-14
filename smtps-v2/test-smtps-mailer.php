a2fa3432def338ee6cccd927e117b13653b8b2cd
NulledUI <61349231+NulledUI@users.noreply.github.com>
2020-06-14 08:46:37 +0300

    Create test-smtps-mailer.php
    add test script for wrapper in php

---
+++ smtps-v2/test-smtps-mailer.php
@@ -0,0 +1,28 @@
+<?
+
+$from = "email@domain.name";
+$to = "box@mydomain.com";
+$cc= "ccbox@mydomain.com";
+$bcc = "bccbox@mydomain.com";
+
+$header = "From: " . $from . "\r\n".
+$header = "Reply-To: " . $from . "\r\n".
+	        "Content-Type: text/plain; charset=utf-8;\r\n".
+          # we don't need MID here cause wrapper generate it 
+          #"Message-ID: <" . time() . "." . $from . ">\r\n".
+	        "Cc: " . $cc . "\r\n".
+          "Bcc: " . $bcc;
+
+# use it with mail() function  
+#$param = "-f" . $from;
+
+$subject = "The subject";
+$body = "Hello,\n\nThis is the mail body";
+
+if (mail($to, $subject, $body, $header, $param)) {
+echo("<p>Message successfully sent!</p>");
+} else {
+echo("<p>Message delivery failed...</p>");
+}
+
+?>
